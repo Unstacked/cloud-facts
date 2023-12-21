1c8a82456521c8060b7903eac4e9bd86b2a01317
Fred Stoddart <freddiestoddart000@gmail.com>
2023-12-22 00:32:43 +1100

    fix 404 path and add cloud function

--- cdk/app.ts
+++ cdk/app.ts
@@ -3,14 +3,15 @@ import 'source-map-support/register';
 import {
   App,
   Stack,
-  type StackProps,
+  RemovalPolicy,
+  aws_certificatemanager as acm,
   aws_cloudfront as cf,
   aws_cloudfront_origins as cfo,
-  aws_s3 as s3,
   aws_route53 as r53,
   aws_route53_targets as r53t,
-  aws_certificatemanager as acm,
+  aws_s3 as s3,
   aws_s3_deployment as s3d,
+  type StackProps,
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -32,12 +33,39 @@ class CloudFactsStack extends Stack {
       validation: acm.CertificateValidation.fromDns(zone),
     });
 
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket');
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
     const distribution = new cf.Distribution(this, 'WebDistrobution', {
       defaultBehavior: {
         origin: new cfo.S3Origin(websiteBucket),
         viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        functionAssociations: [
+          {
+            function: new cf.Function(this, 'UrlRewriteFunction', {
+              code: cf.FunctionCode.fromInline(`function handler(event) {
+              var request = event.request;
+              var uri = request.uri;
+            
+              // Check whether the URI is missing a file name.
+              if (uri.endsWith('/')) {
+                request.uri += 'index.html';
+              }
+              // Check whether the URI is missing a file extension.
+              else if (!uri.includes('.')) {
+                request.uri += '/index.html';
+              }
+            
+              return request;
+            }`),
+            }),
+            eventType: cf.FunctionEventType.VIEWER_REQUEST,
+          },
+        ],
       },
       certificate,
       domainNames: [domainName],
@@ -45,12 +73,12 @@ class CloudFactsStack extends Stack {
       errorResponses: [
         {
           httpStatus: 404,
-          responsePagePath: '404.html',
+          responsePagePath: '/404.html',
           responseHttpStatus: 404,
         },
         {
           httpStatus: 403,
-          responsePagePath: '404.html',
+          responsePagePath: '/404.html',
           responseHttpStatus: 404,
         },
       ],
